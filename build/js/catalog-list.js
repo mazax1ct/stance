f67883574829ebb9a608fd0096c201c1dd444a98
mazax1ct <vova@giveandget.ru>
2021-04-26 11:59:10 +0300

    catalog-item fix

--- build/js/catalog-list.js
+++ build/js/catalog-list.js
@@ -2,7 +2,7 @@ $(document).on('mouseenter', '.catalog-item__image-toggler', function () {
   $(this).parent('.catalog-item__image-togglers').find('.catalog-item__image-toggler').removeClass('is-active');
   $(this).addClass('is-active');
   $(this).closest('.catalog-item__image-block').find('img').attr('src', $(this).attr('data-src'));
-  $(this).closest('.catalog-item__image-block').find('img').attr('setsrc', $(this).attr('data-setsrc'));
+  $(this).closest('.catalog-item__image-block').find('img').attr('srcset', $(this).attr('data-srcset'));
   return false;
 });
 
